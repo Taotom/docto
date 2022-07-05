4ffef69c3f023f467df2429e61dbbcdc1132f169
Calliopée <calliopeedes@hotmail.fr>
2022-07-05 14:38:21 +0200

    medocs.json

--- src/doctoAPI.js
+++ src/doctoAPI.js
@@ -7,6 +7,20 @@ const API_URL = "http://localhost:5000";
 const country = "Angleterre"
 const country_id = 1
 
+
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("./medicaments.json");
+    xobj.open('GET', './medicaments.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState === 4 && xobj.status == "200") {
+            callback(JSON.parse(xobj.responseText));
+        }
+    };
+    xobj.send(null);
+}
+
+
 function doctoServer(port) {
     app.use(express.json());
 
@@ -15,10 +29,21 @@ function doctoServer(port) {
         try {
             console.log(req.query.name);
             //res.json(req.query.name);
-            const fs = require('fs');
-            const data = fs.readFileSync('./medicaments.txt',
-                {encoding:'utf8', flag:'r'});
-            const arr = data.split(/\r?\n/);
+
+            //const fs = require('fs');
+            const data = require("./medicaments.json");
+            console.log(data);
+
+            console.log("tes");
+            //const data = fs.readFileSync('./medicaments.json',
+                //{encoding:'utf8', flag:'r'});
+
+            openJSONas
+
+
+
+
+            /*const arr = data.split(/\r?\n/);
 
             var found = 0;
             for (var cursor in arr)
@@ -32,15 +57,10 @@ function doctoServer(port) {
                     break;
                 }
             }
-
             if (found == 0)
                 res.json("no result");
-
             //console.log(arr[0]);
-
-            //console.log(data);
-
-
+            //console.log(data);*/
         }
         catch (error) {
             res.status(404).send("Not found");
@@ -50,7 +70,6 @@ function doctoServer(port) {
     return app.listen(port, () => {
         console.log(`Docto API listening at http://localhost:${port}`);
     });
-
 
 }
 
